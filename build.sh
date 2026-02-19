56e79a511e847c5038d96c44035d58953575b67e
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-02-19 19:09:59 +0700

    part of your build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# Update this part of your build.sh
 if [ "$CREATE_SUPERUSER" ]; then
-  python manage.py createsuperuser --no-input || true
+  python manage.py createsuperuser \
+    --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
 fi
