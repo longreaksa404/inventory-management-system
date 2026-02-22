a96a9e902ad99c802ecf65c9ca94ceff4f624191
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-02-22 19:40:24 +0700

    update redis url

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,12 +2,18 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
 CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
 CELERY_BROKER_USE_SSL = None
 CELERY_REDIS_BACKEND_USE_SSL = None
 CELERY_TASK_ALWAYS_EAGER = False
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 SWAGGER_USE_COMPAT_RENDERERS = False
