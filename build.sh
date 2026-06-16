955f440a2049227194d217433607b3c16ac5683e
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-06-16 13:48:03 +0700

    fix: use shell -c instead of heredoc for superuser creation

--- build.sh
+++ build.sh
@@ -5,28 +5,18 @@ python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 
 if [ "$CREATE_SUPERUSER" = "True" ]; then
-  python manage.py shell << END
+    python manage.py shell -c "
 from apps.accounts.models import CustomUser
 import os
-
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-first_name = os.environ.get('DJANGO_SUPERUSER_FIRST_NAME')
-last_name = os.environ.get('DJANGO_SUPERUSER_LAST_NAME')
-phone_number = os.environ.get('DJANGO_SUPERUSER_PHONE_NUMBER', '')
-username = email
-
+first_name = os.environ.get('DJANGO_SUPERUSER_FIRST_NAME', 'Admin')
+last_name = os.environ.get('DJANGO_SUPERUSER_LAST_NAME', 'User')
+phone = os.environ.get('DJANGO_SUPERUSER_PHONE_NUMBER', '')
 if not CustomUser.objects.filter(email=email).exists():
-    CustomUser.objects.create_superuser(
-        email=email,
-        username=username,
-        first_name=first_name,
-        last_name=last_name,
-        phone_number=phone_number,
-        password=password,
-    )
-    print("Superuser created.")
+    CustomUser.objects.create_superuser(email=email, username=email, first_name=first_name, last_name=last_name, phone_number=phone, password=password)
+    print('Superuser created.')
 else:
-    print("Superuser already exists.")
-END
+    print('Superuser already exists.')
+"
 fi
