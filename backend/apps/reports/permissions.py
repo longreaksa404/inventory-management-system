34dd6beaaddf3d4f507e353d6deaa36966e1fd63
longreaksa404 <longchansamanakreaksa@gmail.com>
2026-07-02 11:25:09 +0700

    add customeize permission for admin panel

---
+++ backend/apps/reports/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+def has_perm(user, perm):
+    if not user or not user.is_authenticated:
+        return False
+    if user.is_superuser:
+        return True
+    return user.has_perm(perm)
+
+
+class ReportPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return has_perm(request.user, "reports.view_report")
+        if request.method == "POST":
+            return has_perm(request.user, "reports.add_report")
+        if request.method in ["PUT", "PATCH"]:
+            return has_perm(request.user, "reports.change_report")
+        if request.method == "DELETE":
+            return has_perm(request.user, "reports.delete_report")
+        return False
